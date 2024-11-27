f39578e0f03e5e0ee24f41483d34f8affecb8e06
Yasemin Bridges <y.bridges@qmul.ac.uk>
2024-11-27 12:41:02 +0000

    implement runner implementation

--- src/pheval_amelie/runner.py
+++ src/pheval_amelie/runner.py
@@ -5,6 +5,9 @@ from pathlib import Path
 
 from pheval.runners.runner import PhEvalRunner
 
+from pheval_amelie.post_process.post_process import post_process_results_format
+from pheval_amelie.run.run import run
+
 
 @dataclass
 class AmeliePhEvalRunner(PhEvalRunner):
@@ -24,7 +27,13 @@ class AmeliePhEvalRunner(PhEvalRunner):
     def run(self):
         """Run."""
         print("running")
+        run(
+            testdata_dir=self.testdata_dir,
+            tool_input_commands_dir=self.tool_input_commands_dir,
+            raw_results_dir=self.raw_results_dir,
+        )
 
     def post_process(self):
         """Post Process."""
         print("post processing")
+        post_process_results_format(raw_results_dir=self.raw_results_dir, output_dir=self.output_dir)
